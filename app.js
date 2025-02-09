7a9518b92b6364c9e26d863ad50ca5042b08082e
Shayan-Webdeveloper <shayandec2@gmail.com>
2025-02-09 22:45:43 +0500

    Task Done

---
+++ app.js
@@ -0,0 +1,4 @@
+var mass = prompt("Enter mass in kg");
+var acceleration = prompt("Enter acceleration");
+var force = mass * acceleration;
+document.write("<h1 style='font-size: 50px; text-align: center;'>Force applied is: " + force + " N</h1>");
